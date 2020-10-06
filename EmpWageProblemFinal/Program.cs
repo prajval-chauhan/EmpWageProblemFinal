8fcd80a8d24ae3a31773dde070c81b21846b49ad
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-06 19:00:34 +0530

    seperate method for calculating attendance

--- EmpWageProblemFinal/Program.cs
+++ EmpWageProblemFinal/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace EmpWageProblemFinal
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Computation Program\n");
+            Program empAtt = new Program();
+            empAtt.EmpAttendace();
+
+
+            
+
+        }
+        //Employee Attendance Check
+        public void EmpAttendace()
+        {
+            
+            Random randomNum = new Random();
+            int attendance = randomNum.Next(0, 3);
+
+            switch (attendance)
+            {
+                case 1:
+                    Console.WriteLine("Employee is present");
+                    break;
+                case 2:
+                    Console.WriteLine("Employee is working part time");
+                    break;
+                default:
+                    Console.WriteLine("Employee is absent");
+                    break;
+
+            }
         }
     }
 }
