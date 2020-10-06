9aaf1ff04b232010fb854043f8f851571621c508
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-06 18:39:43 +0530

    Welcome Message Added

---
+++ EmpWageProblemFinal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageProblemFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Computation Program\n");
+        }
+    }
+}
